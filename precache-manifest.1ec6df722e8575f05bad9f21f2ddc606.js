785a16959331cbba099a370986c51877e7bc3f2e
Sam Wheeling <anchormansam@gmail.com>
2020-01-08 12:03:17 -0500

    Updates

---
+++ precache-manifest.1ec6df722e8575f05bad9f21f2ddc606.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376faaaae143df1524ceaba4d91fa523",
+    "url": "/newblog/index.html"
+  },
+  {
+    "revision": "d94d3cc59b0e0fe29f60",
+    "url": "/newblog/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c57ba99af8afa064f7bb",
+    "url": "/newblog/static/css/main.67fb619e.chunk.css"
+  },
+  {
+    "revision": "d94d3cc59b0e0fe29f60",
+    "url": "/newblog/static/js/2.d44219ba.chunk.js"
+  },
+  {
+    "revision": "fcbc5feae2b40cf6a2e8baf0a5a20149",
+    "url": "/newblog/static/js/2.d44219ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c57ba99af8afa064f7bb",
+    "url": "/newblog/static/js/main.26943ef5.chunk.js"
+  },
+  {
+    "revision": "998ef5de49fb8e1767d8",
+    "url": "/newblog/static/js/runtime-main.eebb6c7f.js"
+  }
+]);
